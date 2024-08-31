8b53786dff44fe2a3d0724d2f6a0b36fe279485e
T34 <t34.dev@gmail.com>
2024-08-31 13:22:43 +0300

    hotfix path for test

--- .make/test.mk
+++ .make/test.mk
@@ -2,9 +2,9 @@ test:
 	@mkdir -p $(TEMP_DIR)/.temp
 	@CGO_ENABLED=0 go test \
 	. \
-	-coverprofile=$(TEMP_DIR)/.temp/coverage-report.out -covermode=count
-	@go tool cover -html=$(TEMP_DIR)/.temp/coverage-report.out -o $(TEMP_DIR)/.temp/coverage-report.html
-	@go tool cover -func=$(TEMP_DIR)/.temp/coverage-report.out
+	-coverprofile=$(TEMP_DIR)/coverage-report.out -covermode=count
+	@go tool cover -html=$(TEMP_DIR)/coverage-report.out -o $(TEMP_DIR)/coverage-report.html
+	@go tool cover -func=$(TEMP_DIR)/coverage-report.out
 
 
 .PHONY: test
